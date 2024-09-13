45987ee4959994eea658b3fc7715954315eb7afa
virus4it <143338599+virus4it@users.noreply.github.com>
2024-09-13 17:07:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def send_email(message, recipient, *, sender="university.help@gmail.com"):
-    # Проверка на корректность email (наличие "@" и окончание на ".com", ".ru", ".net")
+
     if "@" not in recipient or not recipient.endswith(('.com', '.ru', '.net')):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
         return
@@ -7,19 +7,19 @@ def send_email(message, recipient, *, sender="university.help@gmail.com"):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
         return
 
-    # Проверка на отправку самому себе
+
     if sender == recipient:
         print("Нельзя отправить письмо самому себе!")
         return
 
-    # Проверка на отправителя по умолчанию
+
     if sender == "university.help@gmail.com":
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
     else:
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
 
 
-# Примеры вызова функции
+
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
